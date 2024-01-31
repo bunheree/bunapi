1cfbdfeefa8704399adcdb1d75792cec5f55991f
LOAN Ngo <mrs.loan1999@gmail.com>
2024-01-31 11:57:02 +0700

    Fixed: Build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: config => {
+        config.ignoreWarnings = [
+            { module: /node_modules\/swagger-jsdoc\/src\/utils\.js/ },
+            { file: /node_modules\/swagger-jsdoc\/src\/index\.js/ },
+        ];
+
+        return config;
+    },
+};
 
 export default nextConfig;
--- src/app/api-doc/page.tsx
+++ src/app/api-doc/page.tsx
@@ -3,6 +3,7 @@ import ReactSwagger from '../../components/react-swagger'
 
 export default async function IndexPage() {
     const spec = await getApiDocs()
+
     return (
         <section className="container">
             <ReactSwagger spec={spec} />
